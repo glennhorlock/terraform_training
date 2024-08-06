6c5e048bbff4d0fa4b716bfebfd97df951a2e7dc
glennhorlock <glenn79horlock@gmail.com>
2024-08-06 20:00:40 +0000

    send http get with terraform

---
+++ http-tf/outputs.tf
@@ -0,0 +1,11 @@
+# produces an output value named "space_heroes"
+output "space_heroes" {
+  description = "API that documents folks in space"
+  value       = data.http.iss.response_body
+}
+
+# produces legal JSON output value named "space_heroes_json"
+output "space_heroes_json" {
+  description = "API that documents folks in space"
+  value       = jsondecode(data.http.iss.response_body)    // note the jsondecode()
+}    
