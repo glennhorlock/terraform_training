82b0823067133e118f13e02bc7e871fd0ee82ce4
glennhorlock <glenn79horlock@gmail.com>
2024-08-06 13:58:57 +0000

    workspaces

---
+++ myworkspace/main.tf
@@ -0,0 +1,29 @@
+# main.tf
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.22.0"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_image" "nginx" {
+  name         = "nginx:1.23.4"
+  keep_locally = true    // keep image after "destroy"
+}
+
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.image_id
+  # here we removed the name "tutorial" for the container
+  # and replace it with a call to a variable
+  # name  = "tutorial"
+  name = var.container_name
+  ports {
+    # internal and external are now defined by variables
+    internal = var.internal_port
+    external = var.external_port
+  }
+}
